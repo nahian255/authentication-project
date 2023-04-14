61aa5871620b7d3534fa665fa9aeda0970dc3b06
nahian255 <nahimn253@gmail.com>
2023-04-14 21:23:25 +0600

    nedd to added firebase

---
+++ src/Component/SimpleLogin.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SimpleLogin = () => {
+    return (
+        <div>
+            <h1>Doing this previous style</h1>
+        </div>
+    );
+};
+
+export default SimpleLogin;
---
+++ src/Firebase/firebase.init.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAoqAEvTzTEXooY2SMg5TDTFOsf7dQ4hEo",
+    authDomain: "authentication-project-33d99.firebaseapp.com",
+    projectId: "authentication-project-33d99",
+    storageBucket: "authentication-project-33d99.appspot.com",
+    messagingSenderId: "855751678677",
+    appId: "1:855751678677:web:004b9b4ca2547374dbe05c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
